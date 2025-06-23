48384bc43f0143017715da1d53241ebf07d94271
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-06-23 19:06:30 +0530

    oauth1

---
+++ server/config/google.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/users");
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id,done) =>{
+    try{
+        const user = await User.findById(id);
+        done(null, user);
+    }catch(err){
+        done(err);
+    }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = await new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails?.[0]?.value || "",
+            profilePicture: profile.photos?.[0]?.value,
+            provider: "google",
+          }).save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load environment variables
+dotenv.config();
+
+// Passport Strategies
+require("./config/google");
+
+// Route files
+const authRoutes = require("./routes/auth");
+
+// Initialize app
+const app = express();
+
+// Enable CORS for frontend
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Database connected");
+
+    console.log("📦 Using database:", mongoose.connection.name);
+
+  });
+
+// Session Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || "defaultsecret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, 
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24, 
+  },
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>ExchangeIQ Auth</h1>
+    <a href='/auth/google'>Login with Google</a><br>
+  `);
+});
+
+app.use("/auth", authRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on PORT: ${PORT}`);
+});
