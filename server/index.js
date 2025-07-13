16aaf372c68e5b0805e5874d9345a0aee85d848b
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-07-13 17:45:12 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -61,8 +61,8 @@ const sessionMiddleware = session({
   cookie: {
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-    sameSite: "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "None",
   },
 });
 
