ad3eb8bb2699eb7ecedfe203c99c4b19169d0f84
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-07-16 10:24:03 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: "None",
